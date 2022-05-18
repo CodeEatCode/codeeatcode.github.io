a93fd94052f9c141a14f8f3777be66866593f298
Code Eat Code <1080238+ambersariya@users.noreply.github.com>
2022-05-18 14:22:07 +0100

    fix: remove trailingSlash: false, it's needed for GHPages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "codeeatcode", // Usually your GitHub org/user name.
     projectName: "codeeatcode.github.io", // Usually your repo name.
-    trailingSlash: false,
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
