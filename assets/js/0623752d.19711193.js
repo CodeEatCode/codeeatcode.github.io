0d90a134581c7859a2cc7b5f87b2518f687e7ebe
ambersariya <ambersariya@users.noreply.github.com>
2022-05-19 11:22:16 +0000

    deploy: 77e2235e37b25fbd2dcecbb1b3ef08e2594e9c5e

---
+++ assets/js/0623752d.19711193.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcodeeatcode_github_io=self.webpackChunkcodeeatcode_github_io||[]).push([[727],{5044:function(e){e.exports=JSON.parse('{"label":"containerisation","permalink":"/tags/containerisation","allTagsPath":"/tags","count":1}')}}]);
---
+++ assets/js/5a8efaa1.e116f610.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcodeeatcode_github_io=self.webpackChunkcodeeatcode_github_io||[]).push([[23],{223:function(e){e.exports=JSON.parse('{"permalink":"/tags/containerisation","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
